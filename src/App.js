f84c2a0913e3b8a39aa6522f5843637475ead672
CVSCharan <charan.cvs@gmail.com>
2022-08-07 18:58:12 +0530

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div class="bg-container">
+      <div class="container">
+        <div class="row">
+          <div class="col-12">
+            <h1 class="heading">Products</h1>
+          </div>
+          <div class="col-12 col-md-6">
+            <div class="card-container d-flex flex-column justify-content-end">
+              <h1 class="cardheading">VR Headset</h1>
+              <p class="cardprice">Rs 75,000</p>
+              <button class="butn btn-outline-light">Buy Now</button>
+            </div>
+          </div>
+          <div class="col-12 col-md-6">
+            <div class="card1-container d-flex flex-column justify-content-end">
+              <h1 class="cardheading">VR Headset</h1>
+              <p class="cardprice">Rs 75,000</p>
+              <button class="butn btn-outline-light">Buy Now</button>
+            </div>
+
+            <div class=" col-12 d-flex flex-row justify-content-end">
+              <button class="butn1 btn-primary mt-3">View All</button>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
